e03a2fde905ee04a92174cafc025100d07a8ff42
Sai Nitish Reddy <sainitishreddykapa@gmail.com>
2022-12-07 12:39:11 +0000

    created greeting contract

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,10 +1,11 @@
 //class is used to create a asmartcontract
-import { storage } from "near-sdk-as" ;
+import { storage,Context } from "near-sdk-as" ;
 class greeting{
     getGreeting(accountId: string): string | null{
         return storage.get<string>(accountId,"Greeting is not available");
     }
-    setGreeting(){
+    setGreeting(greeting:string){
+        storage.set(Context.sender,greeting);
 
     }
 }
